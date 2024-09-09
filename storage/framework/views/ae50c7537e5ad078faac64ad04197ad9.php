21adb1c3fceda1651f077d7adec39c48325f09a1
Farah Dwi Ameliani <99456071+Farahdwi21@users.noreply.github.com>
2024-09-09 21:36:21 +0700

    Add files via upload

---
+++ storage/framework/views/ae50c7537e5ad078faac64ad04197ad9.php
@@ -0,0 +1,37 @@
+<footer class="footer border-top">
+    <div class="container pt-4 pb-4">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="row">
+                    <!-- Logo -->
+                    <div class="col-md-4 col-12 mt-5 text-center text-md-start mb-3 mb-md-0">
+                        <img src="asset/logo.png" width="auto" height="28" alt="logo">
+                    </div>
+
+                    <!-- Menu Links -->
+                    <div class="col-md-4 col-12 text-center text-md-start mb-3 mb-md-0">
+                        <ul class="list-unstyled menu-footer">
+                            <li><a href="">Menu (Menyesuaikan)</a></li>
+                            <li><a href="">Menu (Menyesuaikan)</a></li>
+                            <li><a href="">Menu (Menyesuaikan)</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- Kontak -->
+                    <div class="col-md-4 col-12 text-center text-md-start mb-3 mb-md-0">
+                        <h5 class="judul-footer">Kontak Kami</h5>
+                        <div class="contact">
+                            <i class="fa-solid fa-phone social-icon"></i>
+                            <a class="m-2 text-black text-decoration-none">+628112071302</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center p-3" style="background-color: #EAE7B1; color: black; border-top: 1px solid #3C6255;">
+        © Media Akselerasi 2024. All rights reserved.
+    </div>
+</footer>
+<?php /**PATH C:\xampp\htdocs\quranInspirasi\resources\views/footer.blade.php ENDPATH**/ ?>
